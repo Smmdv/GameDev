bebbcf301c3862b8da8e4200ea9ef060b0bdd863
Smmdv <Semdevries01@gmail.com>
2023-06-15 11:57:26 +0200

    Descend keybind
    Met de "S" key descend je nu sneller naar de ground. Deze keybind werkt alleen als je niet de grond aanraakt

--- super goin to the moon/Assets/playermovement.cs
+++ super goin to the moon/Assets/playermovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class playermovement : MonoBehaviour
 {
     private bool isGrounded; // Flag to check if the player is on the ground
+    private bool isDescending; // Flag to check if the player is descending
 
     // Start is called before the first frame update
     void Start()
     {
         isGrounded = true; // Assume the player starts on the ground
+        isDescending = false;
     }
 
     // Update is called once per frame
@@ -18,8 +20,28 @@ public class playermovement : MonoBehaviour
         if (Input.GetKey("space") && isGrounded)
         {
             // Apply jump force only when the player is on the ground
-           GetComponent<Rigidbody2D>().velocity = new Vector3(0, 5, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 8, 0);
             isGrounded = false; // Set isGrounded to false to prevent multiple jumps
+        }
+
+        // Check if the player is not grounded and holding the "s" key
+        if (!isGrounded && Input.GetKey("s"))
+        {
+            isDescending = true;
+        }
+        else
+        {
+            isDescending = false;
+        }
+    }
+
+    // FixedUpdate is called at a fixed interval
+    void FixedUpdate()
+    {
+        if (isDescending)
+        {
+            // Apply a higher downward force to make the player descend quicker
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -40));
         }
     }
 
@@ -29,6 +51,7 @@ public class playermovement : MonoBehaviour
         if (collision.gameObject.CompareTag("ground"))
         {
             isGrounded = true; // Set isGrounded to true when the player touches the ground
+            isDescending = false; // Reset the descending flag
         }
     }
 }
